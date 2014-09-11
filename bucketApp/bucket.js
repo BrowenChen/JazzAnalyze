20216d3e11652a28f1da53c73b52ba34fe010440
Browenchen <owenchen@berkeley.edu>
2014-09-11 10:01:21 -0700

    adding in the interval exercise thing

---
+++ bucketApp/bucket.js
@@ -0,0 +1,20 @@
+// Generate random intervals. Mode selection to change between scale types and practice playing those intervals over a set of chord changes
+
+
+
+var generateChordProgression = function(){
+	var suitableChords = ['A', 'B', 'C', 'E', 'D', 'F', 'G'];
+	var chords = [];
+
+
+
+
+}
+
+var randomRange = function(max, min){
+	return Math.floor(Math.random() * (max - min));
+
+}
+
+// console.log(randomRange(8, 0));
+
